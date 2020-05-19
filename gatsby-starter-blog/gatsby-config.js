67b2eab977032c12119d238c5e17907156b9e59c
tamakiii <tamakiii@users.noreply.github.com>
2020-05-19 12:00:42 +0900

    me

--- gatsby-starter-blog/gatsby-config.js
+++ gatsby-starter-blog/gatsby-config.js
@@ -2,13 +2,13 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `tamakiii`,
+      summary: `who lives and works in Shibuya uilding useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://twitter.com/tamakiii`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `tamakiii`,
     },
   },
   plugins: [
